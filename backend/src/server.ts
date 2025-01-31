3ce28d18cbb854f19bd95175b0223332acbbc436
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-01-31 16:48:50 -0300

    use routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import routes from "./routes";
 
 dotenv.config();
 
@@ -8,9 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API funcionando!");
-});
+app.use(routes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
