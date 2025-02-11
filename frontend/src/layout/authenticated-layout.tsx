51f14f727830cdfc245c2aff7ad83ffe9b286e13
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-02-11 18:02:46 -0300

    Adicionado user no header

--- frontend/src/layout/authenticated-layout.tsx
+++ frontend/src/layout/authenticated-layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Sidenav from "./sidenav";
+import Header from "./header";
 
 const AuthenticatedLayout = () => {
     return (
@@ -9,7 +10,7 @@ const AuthenticatedLayout = () => {
 
             {/* Conteúdo da página */}
             <div className="flex-1 flex flex-col h-screen">
-                <div className="w-full p-4 bg-[#1E1E1E] border-b-1 border-[#333333]">teste</div>
+                <Header />
                 <main className="flex-1 flex justify-center p-6 w-full max-w-7xl mx-auto">
                     <Outlet />
                 </main>
---
+++ frontend/src/layout/header.tsx
@@ -0,0 +1,17 @@
+import { User } from 'lucide-react';
+
+export default function Header() {
+    return (
+        <div className="bg-[#1E1E1E] border-b-1 border-[#333333] p-4 flex justify-between items-center">
+            <div className="text-white text-lg font-semibold">
+                Meu App
+            </div>
+
+            <div className="flex justify-end">
+                <div className="flex justify-center items-center w-10 h-10 bg-[#FF9800] rounded-full">
+                    <User className="w-6 h-6" />
+                </div>
+            </div>
+        </div>
+    );
+}
