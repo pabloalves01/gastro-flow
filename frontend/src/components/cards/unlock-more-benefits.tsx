ae8b6df93da8f0a2c010fb6bcd0a7b4b12ac345d
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-02-14 20:29:05 -0300

    Update unlock-more-benefits.tsx

--- frontend/src/components/cards/unlock-more-benefits.tsx
+++ frontend/src/components/cards/unlock-more-benefits.tsx
@@ -2,7 +2,7 @@ import { Button } from '../ui/catalyst/button'
 
 export default function UnlockMoreBenefits() {
     return (
-        <div className="flex flex-col py-12 px-6 gap-2 items-center justify-center bg-[#141414] border border-[#333333] p-6 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+        <div className="flex flex-col py-12 px-6 gap-2 items-center justify-center bg-[#141414] border border-[#333333] p-6 rounded-xl">
             <div className='text-center'>
                 <div className="text-white text-md font-semibold">Atualize seu plano para liberar mais recursos!</div>
                 <div className="text-[#A1A1A1] text-sm font-regular mb-4">
