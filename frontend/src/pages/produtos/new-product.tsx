ca827d1087bf0318e1231aaac720cf70e3720d3c
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-02-17 17:12:22 -0300

    Update new-product.tsx

--- frontend/src/pages/produtos/new-product.tsx
+++ frontend/src/pages/produtos/new-product.tsx
@@ -10,15 +10,19 @@ const productTabs = [
 ];
 
 function ProductForm() {
-  const [activeTab, setActiveTab] = useState('Geral');
+  const [activeTab, setActiveTab] = useState('dados gerais');
 
   return (
     <div>
       <Tabs tabs={productTabs} activeTab={activeTab} onTabChange={setActiveTab} />
       <div className="mt-4">
-        {activeTab === 'Geral' && <div>
-          <SectionText title="Dados Gerais" subtitle='teste' />
-        </div>}
+        {activeTab === 'dados gerais' &&
+          <div>
+            <div className='pb-4'>
+              <SectionText title="Dados Gerais do Produto" subtitle='Preencha as informações para garantir um controle preciso do produto.' />
+            </div>
+            Começo dos inputs aqui
+          </div>}
       </div>
 
     </div>
