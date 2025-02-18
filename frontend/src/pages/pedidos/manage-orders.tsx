c76ba88fac0582a418b8ce88119eb9ae1c2652f9
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-02-18 17:44:16 -0300

    Add badge modal visualizar pedido

--- frontend/src/pages/pedidos/manage-orders.tsx
+++ frontend/src/pages/pedidos/manage-orders.tsx
@@ -268,6 +268,13 @@ export function ManageOrders() {
       <Alert open={isOpenView} onClose={() => setIsOpenView(false)}>
         <div className="flex w-full flex-wrap items-end justify-between gap-4 border-b border-zinc-950/10 pb-4 dark:border-white/10">
           <Heading>Pedido #00001</Heading>
+          {selectedUser?.status === "Em andamento" ? (
+            <Badge color="blue">Em andamento</Badge>
+          ) : selectedUser?.status === "Finalizado" ? (
+            <Badge color="green">Finalizado</Badge>
+          ) : (
+            <Badge color="yellow">Pendente</Badge>
+          )}
           {/* <div className="flex gap-4">
             <Button outline>Refund</Button>
             <Button>Resend invoice</Button>
