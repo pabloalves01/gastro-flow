aff0c2229370af55fb14475f79dcc479b0298d8e
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-02-18 10:45:25 -0300

    Added Loading in Splashscreen

--- frontend/src/pages/splashscreen.tsx
+++ frontend/src/pages/splashscreen.tsx
@@ -1,9 +1,21 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Loader } from "lucide-react";
 
 export default function SplashScreen() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate("/login");
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
     return (
-        <div className="text-white">splashscreen ------------
-            <Link to={"/login"}>Ir para login</Link>
+        <div className="flex flex-col items-center justify-center h-screen text-white text-2xl">
+            <Loader className="w-10 h-10 text-white mb-4 animate-spin" style={{ animationDuration: "2s" }} />
         </div>
-    )
+    );
 }
