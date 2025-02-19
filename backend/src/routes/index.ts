0892183780c6523843c6fb844661181d31d8beff
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-02-19 09:04:40 -0300

    Update index.ts

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,7 @@
 // src/routes/index.ts
 import { Router } from "express";
-import { obterDadosTeste } from "../controllers/transactionController";
-import { obterUsuarios } from "../controllers/userController"; 
+import { obterUsuarios } from "../controllers/userController";
 const router = Router();
 router.get("/usuarios", obterUsuarios); // Criando a rota para buscar usuários
-router.get("/teste", obterDadosTeste);  // Certifique-se de que esta rota está configurada
 
 export default router;
