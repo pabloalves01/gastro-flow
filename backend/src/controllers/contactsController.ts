d6fc2db74689323c302cfcb1c5e634ac0cb2fbe5
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2025-02-20 22:20:59 -0300

    Criando dados obrigatorios na tabela contacts

---
+++ backend/src/controllers/contactsController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import Contacts from "../models/contacts";
+
+export const storeContacts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const formData = req.body;
+    const contact = await Contacts.create(formData);
+    res
+      .status(201)
+      .json({ message: "Contato criado com sucesso!", contact });
+  } catch (error) {
+    console.error("Erro ao criar contato:", error);
+    res.status(500).json({ error: "Erro interno ao criar contato." });
+  }
+};
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { obterUsuarios } from "../controllers/userController";
 import { getStates } from "../controllers/common/stateController";
+import { storeContacts } from "../controllers/contactsController";
 import {
   storeCategory,
   getCategories,
@@ -13,7 +14,7 @@ router.get("/usuarios", obterUsuarios);
 router.get("/categories", getCategories);
 router.post("/categories", storeCategory);
 router.delete("/categories/:id", destroyCategory);
-
 router.get("/states", getStates);
+router.post("/contacts", storeContacts);
 
 export default router;
